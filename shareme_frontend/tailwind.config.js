2df809d7ac23e1cbfbacab2a2c3d3f894e1f5fb0
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2024-03-30 19:38:27 +0100

    Tailwind

--- shareme_frontend/tailwind.config.js
+++ shareme_frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './public/index.html'
+  ],
   theme: {
     extend: {
       margin: {
